e7d71d761635136d5741993cc34b69cf1e02943b
MaxHiit <pro.maxcode@gmail.com>
2022-01-15 22:23:14 +0100

    refacto home page | add layout of the second card | add image | add packaging choice page | add transport choice page

--- pages/transport/index.js
+++ pages/transport/index.js
@@ -1,44 +1,39 @@
-import Link from 'next/link'
 import { useStepContext } from '@context/stepContext'
-import { DATA_HOME } from '@utils/mockData'
+import { DATA_TRANSPORT } from '@utils/mockData'
 import routes from '@config/routes'
 import Layout from '@components/Layout'
 import Card from '@components/Card'
 
-const Home = () => {
+const Transport = () => {
 	const { dispatch } = useStepContext()
 
 	const _handleClick = (data, e) => {
 		e.preventDefault()
-		const { title, price, stockage, cubic_meter, src } = data
+		const { title, price, benefits, src } = data
 		dispatch({
-			type: 'ADD_STOCKAGE_STEP',
-			payload: { title, price, stockage, cubic_meter, src },
+			type: 'ADD_TRANSPORT_STEP',
+			payload: { title, price, benefits, src },
 		})
 	}
+
 	return (
-		<Layout pageTitle='De combien d’espace avez-vous besoin ?'>
+		<Layout pageTitle='Comment voulez-vous transporter vos affaires en stockage ?'>
 			<section className='section-page'>
 				<div className='flex flex-column flex-centered'>
 					<div className='card-list flex flex-row flex-centered'>
-						{DATA_HOME.map((data, key) => (
+						{DATA_TRANSPORT.map((data, index) => (
 							<Card
-								key={key + data.id}
+								key={index + data.id}
 								data={data}
-								route={routes.summary}
+								route={routes.insurance}
 								onClickHandle={(e) => _handleClick(data, e)}
 							/>
 						))}
 					</div>
-					<Link href='/'>
-						<a className='link home-link'>
-							Aidez-moi à estimer mon volume de stockage
-						</a>
-					</Link>
 				</div>
 			</section>
 		</Layout>
 	)
 }
 
-export default Home
+export default Transport
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -12,17 +12,19 @@ const Layout = ({ children, pageTitle }) => {
 			</header>
 			<main className='container'>{children}</main>
 			<footer className='container'>
-				<Link href=''>
-					<div className='svg-wrapper'>
-						<svg
-							xmlns='http://www.w3.org/2000/svg'
-							width='24'
-							height='24'
-							viewBox='0 0 24 24'
-						>
-							<path d='M0 12l9-8v6h15v4h-15v6z' />
-						</svg>
-					</div>
+				<Link href='/'>
+					<a href=''>
+						<div className='svg-wrapper'>
+							<svg
+								xmlns='http://www.w3.org/2000/svg'
+								width='24'
+								height='24'
+								viewBox='0 0 24 24'
+							>
+								<path d='M0 12l9-8v6h15v4h-15v6z' />
+							</svg>
+						</div>
+					</a>
 				</Link>
 			</footer>
 		</>
